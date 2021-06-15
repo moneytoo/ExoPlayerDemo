ad3c0ea150bf6e1ba6eed2bdf4695194e4f5de41
Marcel Dopita <m@rcel.cz>
2021-06-16 00:11:37 +0200

    Update

--- app/src/main/java/com/example/exoplayer/MainActivity.java
+++ app/src/main/java/com/example/exoplayer/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.exoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.RenderersFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
@@ -22,13 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         playerView = findViewById(R.id.video_view);
 
-        DefaultTrackSelector trackSelector = new DefaultTrackSelector(this);
-        RenderersFactory renderersFactory = new DefaultRenderersFactory(this)
-                .setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON);
-        player = new SimpleExoPlayer.Builder(this, renderersFactory)
-                .setTrackSelector(trackSelector)
+        playerView.setShowNextButton(false);
+        playerView.setShowPreviousButton(false);
+        playerView.setShowFastForwardButton(false);
+        playerView.setShowRewindButton(false);
+
+        playerView.setShowSubtitleButton(true);
+
+        player = new SimpleExoPlayer.Builder(this)
                 .build();
 
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.test5);
+        MediaItem.Builder mediaItemBuilder = new MediaItem.Builder()
+                .setUri(uri);
+
         playerView.setPlayer(player);
+
+        player.setMediaItem(mediaItemBuilder.build());
+
+        player.prepare();
+
+        playerView.setControllerShowTimeoutMs(-1);
+        playerView.showController();
     }
 }
